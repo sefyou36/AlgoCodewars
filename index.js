66c322f7b62f28a97e6f4c4a51aca37730d99b2a
sefyou36 <rezoukiy@gmail.com>
2023-11-05 15:37:38 +0100

    stringToArray exercice codewars

--- index.js
+++ index.js
@@ -1,59 +1,65 @@
-// const stringToNumber = function(str){
-//     // put your code here
-//     return str - "";
-//     return null ;
-//   }
+const stringToNumber = function(str){
+    // put your code here
+    return str - "";
+    return null ;
+  }
 
 //   //Grasshopper - Grade book
-//   function getGrade (s1, s2, s3) {
-//     var average = Math.round(s1 + s2 + s3)/3;
-//      if(average <= 100 && average >= 90){
-//      return 'A';
-//    } else if(average <= 90 && average >= 80){
-//      return 'B';
-//    } else if(average <= 80 && average >= 70 ){
-//      return 'C';
-//    } else if(average <= 70 && average >= 60){
-//      return 'D';
-//    } else {
-//      return 'F';
-//  }
-//  }
+  function getGrade (s1, s2, s3) {
+    var average = Math.round(s1 + s2 + s3)/3;
+     if(average <= 100 && average >= 90){
+     return 'A';
+   } else if(average <= 90 && average >= 80){
+     return 'B';
+   } else if(average <= 80 && average >= 70 ){
+     return 'C';
+   } else if(average <= 70 && average >= 60){
+     return 'D';
+   } else {
+     return 'F';
+ }
+ }
 
 //Will there be enough space?
-// function enough(cap, on, wait) {
-//     // your code here
+function enough(cap, on, wait) {
+    // your code here
     
-//   if(on + wait <= cap){
-//       return 0
-//       }
-//     else if(on + wait > cap){
-//       return (  (on + wait) - cap )
-//     }
+  if(on + wait <= cap){
+      return 0
+      }
+    else if(on + wait > cap){
+      return (  (on + wait) - cap )
+    }
   
-//   }
+  }
 
 
 // Difference of Volumes of Cuboids
-// function findDifference(a, b) {
-//     //loading...
+function findDifference(a, b) {
+    //loading...
     
-//    var d = 0;
-//     d= a[0]*a[1]*a[2] - b[0]*b[1]*b[2];
-//     if(d < 0){
-//       d= d* -1
-//     }
-//     return d
+   var d = 0;
+    d= a[0]*a[1]*a[2] - b[0]*b[1]*b[2];
+    if(d < 0){
+      d= d* -1
+    }
+    return d
       
-//   }
+  }
 
 // L1: Set Alarm
-// function setAlarm(employed, vacation){
-//     return employed && !vacation 
-// }
+function setAlarm(employed, vacation){
+    return employed && !vacation 
+}
 
 // You only need one - Beginner
 function check(a, x) {
     // your code here
    return a.includes(x)
   }
+
+//   Convert a string to an array
+function stringToArray(string){
+    return string.split(" ")
+                          
+}
