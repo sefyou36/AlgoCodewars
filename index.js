e2c1f78086863240e0a4b99563a6ffb7ee393a11
sefyou36 <rezoukiy@gmail.com>
2023-11-05 15:08:17 +0100

    difference of volume exercice codewars

--- index.js
+++ index.js
@@ -4,18 +4,45 @@
 //     return null ;
 //   }
 
-  //Grasshopper - Grade book
-  function getGrade (s1, s2, s3) {
-    var average = Math.round(s1 + s2 + s3)/3;
-     if(average <= 100 && average >= 90){
-     return 'A';
-   } else if(average <= 90 && average >= 80){
-     return 'B';
-   } else if(average <= 80 && average >= 70 ){
-     return 'C';
-   } else if(average <= 70 && average >= 60){
-     return 'D';
-   } else {
-     return 'F';
- }
- }
+//   //Grasshopper - Grade book
+//   function getGrade (s1, s2, s3) {
+//     var average = Math.round(s1 + s2 + s3)/3;
+//      if(average <= 100 && average >= 90){
+//      return 'A';
+//    } else if(average <= 90 && average >= 80){
+//      return 'B';
+//    } else if(average <= 80 && average >= 70 ){
+//      return 'C';
+//    } else if(average <= 70 && average >= 60){
+//      return 'D';
+//    } else {
+//      return 'F';
+//  }
+//  }
+
+//Will there be enough space?
+// function enough(cap, on, wait) {
+//     // your code here
+    
+//   if(on + wait <= cap){
+//       return 0
+//       }
+//     else if(on + wait > cap){
+//       return (  (on + wait) - cap )
+//     }
+  
+//   }
+
+
+// Difference of Volumes of Cuboids
+function findDifference(a, b) {
+    //loading...
+    
+   var d = 0;
+    d= a[0]*a[1]*a[2] - b[0]*b[1]*b[2];
+    if(d < 0){
+      d= d* -1
+    }
+    return d
+      
+  }
